e215b8e7b44a4de487cbca633e259fe2c388df99
Patryk <psambek@gmail.com>
2022-03-09 19:00:52 +0100

    Project setup

---
+++ src/features/AppContainer/AppContainer.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './AppContainer.scss'
+
+function AppContainer() {
+  return (
+    <div className='app-container'>
+      <div className='bg-container-left'>
+        <div className='top-left'></div>
+        <div className='bot-left'></div>
+      </div>
+      <div className='main-content'>
+
+      </div>
+      <div className='bg-container-right'>
+        <div className='top-right'></div>
+        <div className='bot-right'></div>
+      </div>
+    </div>
+      )
+}
+
+export default AppContainer
